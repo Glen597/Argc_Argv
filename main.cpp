117151641fb8c569adad4688987b423ff0bd92a8
unknown <mboleatebabe80110@th-nuernberg.de>
2022-07-23 02:06:17 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+//Argc: Argument count
+//Argv: Argument Value
+int main(int argc,char **argv) 
+{
+    for (int i = 0; i <argc ; ++i)
+    {
+        std::cout<<argv[i]<<std::endl;
+    }
+    if (argc > 1 )
+    {
+        const char *arg1 =argv[1];
+        std::cout<<arg1<<std::endl;
+
+        int arg2 =std::atoi(argv[2]);
+        std::cout<<arg2<<std::endl;
+    }
+    return 0;
+}
